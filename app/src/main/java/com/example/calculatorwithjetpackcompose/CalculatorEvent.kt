cf96607760062e40d6f30c130b418a0c636d2e0e
Mohammad Tarek Hendi <t.hindi@outlook.com>
2023-10-24 17:25:08 +0800

    * Fix calculatorEvent Class.

--- app/src/main/java/com/example/calculatorwithjetpackcompose/CalculatorEvent.kt
+++ app/src/main/java/com/example/calculatorwithjetpackcompose/CalculatorEvent.kt
@@ -1,10 +1,12 @@
 package com.example.calculatorwithjetpackcompose
 
 sealed class CalculatorEvent {
+
     data class Number(val number: Int) : CalculatorEvent()
     data class Operation(val operation: CalculatorOperation) : CalculatorEvent()
     object Clear : CalculatorEvent()
     object Delete : CalculatorEvent()
     object Decimal : CalculatorEvent()
     object Calculator : CalculatorEvent()
+
 }
